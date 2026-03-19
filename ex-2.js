2cce79c18a7c458f02e8fa8c9f306e2b5b1b4c34
Chindurex <chinnaracha@gmail.com>
2026-03-19 22:58:39 +0700

    add answer ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,15 @@
 //Exercise 2
-let lightBulbStatus = "On";
+let lightBulbStatus = "Test";
 
 // Start coding here.
+if (lightBulbStatus === "On") { 
+    console.log("Light bulb is On.");
+} else if (lightBulbStatus === "Broken"){
+    console.log("Light bulb is Broken.")
+} else if (lightBulbStatus === "Off"){
+    console.log("Light bulb is Off.");
+} else {
+    console.log("Please choose the correct input (On/Off/Broken)");
+
+}
+console.log(lightBulbStatus)
